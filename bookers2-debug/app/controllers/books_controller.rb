f19fc4c94bb78bd15abde168828416e1a0a1c022
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-04-02 03:48:26 +0000

    [Finish]

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -2,9 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -15,13 +18,21 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    @user = @book.user_id
+    if @user == current_user.id
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
+  
+  
 
 
 
@@ -30,20 +41,20 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- bookers2-debug/app/models/book.rb
+++ bookers2-debug/app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
-	has_many :user
+	
+	belongs_to :user
+	# has_many :user
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
